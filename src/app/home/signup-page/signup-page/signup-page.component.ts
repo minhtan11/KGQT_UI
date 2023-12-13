74517cae313a7df8212a815971001a602a6f3810
tmtan <tmtan@lacviet.com>
2023-12-13 13:05:40 +0700

    .

--- src/app/home/signup-page/signup-page/signup-page.component.ts
+++ src/app/home/signup-page/signup-page/signup-page.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-signup-page',
@@ -94,6 +96,23 @@ export class SignupPageComponent  implements OnInit {
     this.http.post(environment.apiUrl+'Authencation/register',this.formGroup.value).pipe(takeUntil(this.destroy$)).subscribe((res:any)=>{
       console.log(res);
     })
+  }
+
+  async pick(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    let imageUrl = image.webPath;
+  
+    // Can be set to the src of an image now
+    console.log(imageUrl)
   }
   //#endregion Function  
 
